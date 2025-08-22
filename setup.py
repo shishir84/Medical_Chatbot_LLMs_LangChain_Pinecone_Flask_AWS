87faf96d5e88f99360301d9b2ea381f907e2e20b
shishir <shishirroy1982@gmail.com>
2025-08-23 00:14:30 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_Chatbot_LLMs_LangChain_Pinecone_Flask_AWS",   
+    version="1.0.0",
+    author="Shishir Kumar Roy",
+    description="A Flask application for a medical chatbot using LLMs, LangChain, Pinecone, and AWS services.",
+    packages=find_packages(),
+    install_requires=[]
+)
+       
